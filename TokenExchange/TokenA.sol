fd8e0e1b72b6a8f664068f564e78f58a47dd90a2
misha-dn <mikhail.davidson@gmail.com>
2024-08-20 12:12:02 +0300

    work on errors

--- TokenExchange/TokenA.sol
+++ TokenExchange/TokenA.sol
@@ -2,35 +2,22 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-import "./ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 /*import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";*/
 
 contract TokenA is ERC20 {
-    address private _owner;
+    address public _owner;
 
-    //constructot takes the address of the token exchange contract
+    //constructor takes the address of the token exchange contract
     constructor()
         ERC20("TokenA", "TA")
 
     {
-        //TokenExchange contract is the owner of this contract
         _owner = msg.sender;
-        //mint initial volume of tokens TA to the TokenExchange
-        mint(100);
+        //mint initial volume of tokens TA to the _owner
+        _mint(msg.sender, 100 * 10 ** decimals());
 
     }
 
-    function owner() public view returns (address) {
-        return _owner;
-    }
  
-    modifier onlyOwner() {
-        require(_owner == msg.sender, "UnauthorizedAccount");
-        _;
-    }
-
-    function mint(uint256 value) public onlyOwner{
-        _mint(msg.sender, value * 10 ** decimals());
-    }
-
 }
--- TokenExchange/TokenExchng.sol
+++ TokenExchange/TokenExchng.sol
@@ -4,83 +4,80 @@ pragma solidity ^0.8.20;
 
 import "./TokenA.sol";
 import "./TokenB.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 
 contract TokenExchange{
-    //for simplicity deploy TokenA and TokenB from TokenExchange contract
-    //initially all tokens minted by TokenA() and TokenB() belong to TokenExchange
-    TokenA internal proxyTA = new TokenA();
-    TokenB internal proxyTB = new TokenB();
+
+    using SafeERC20 for *;
+
+    TokenA internal proxyTA;
+    TokenB internal proxyTB;
     mapping(string => uint) public TokenBalance;
 
-    constructor (){
-        //record starting balance of tokens
-        TokenBalance["TA"] = proxyTA.balanceOf(address(this));
-        TokenBalance["TB"] = proxyTB.balanceOf(address(this));        
+    constructor (TokenA adr1, TokenB adr2){
+        //token addresses are passed to the constructor
+        proxyTA = adr1;
+        proxyTB = adr2;
     }
 
     //sells tokenA or tokenB for ethers 1:1
-    function buyTokenForEther(string memory TokenCode) public payable returns(uint){
+    function buyTokenForEther(address _token) public payable returns(int){
         //msg.value = amount of ethers sent by a buyer
         //check which token is wanted  
-        bool _tokenA = keccak256(abi.encodePacked(TokenCode))==keccak256(abi.encodePacked("TA"));
-        bool _tokenB = keccak256(abi.encodePacked(TokenCode))==keccak256(abi.encodePacked("TB"));
-        uint tokens_transfered;
+        bool _tokenA = _token == address(proxyTA);
+        bool _tokenB = _token == address(proxyTB);
+        int  _tokens_transfered = -1;
         if( _tokenA){
-            //check if the TokenExchange has enough tokens TA
+            //check if TokenExchange has enough tokens TA
             require(proxyTA.balanceOf(address(this)) >= msg.value, "Not enough TAs to sell for ethers, reverting");
-            //send msg.value of TAs to the buyer
-            require(proxyTA.transfer(msg.sender, msg.value)==true, "Transfer of TAs failed, reverting");            
+            //send msg.value of TAs to the buyer, automatically reverts on failure
+            proxyTA.safeTransfer(msg.sender, msg.value);            
             //reflect the change in token balance of the TokenExchange
             TokenBalance["TA"] -= msg.value;
-            //check that tokens reached the buyer
-            tokens_transfered = proxyTA.balanceOf(msg.sender);            
+            _tokens_transfered = int(proxyTA.balanceOf(msg.sender));            
         }
         if( _tokenB){
-            //check if the TokenExchange has enough tokens TA
+            //check if TokenExchange has enough tokens TB
             require(proxyTB.balanceOf(address(this)) >= msg.value, "Not enough TBs to sell for ethers, reverting");
-            //send msg.value of TAs to the buyer
-            require(proxyTB.transfer(msg.sender, msg.value)==true, "Transfer of TBs failed, reverting"); 
+            //send msg.value of TBs to the buyer, automatically reverts on failure
+            proxyTB.safeTransfer(msg.sender, msg.value); 
             //reflect the change in token balance of the TokenExchange
             TokenBalance["TB"] -= msg.value;
-            //check that tokens reached the buyer
-            tokens_transfered = proxyTB.balanceOf(msg.sender);                                     
+            _tokens_transfered = int(proxyTB.balanceOf(msg.sender));                                     
         }
-        if(_tokenA == false && _tokenB == false){
-            revert("wrong token code, reverting");
-        }
+        //if wrong token need to revert in order to roll back ethers to the buyer
+        require(_tokens_transfered >= 0, "wrong token, reverting");
 
-        return tokens_transfered;
+        return _tokens_transfered;
     }
 
-    //swaps B tokens for A 1:1
-    function buyA(uint _amount) public returns(bool){
-        //transfer _amount of TBs from msg.sender to TokenExchange
-        //allowance is checked inside transferFrom
-        require(proxyTB.transferFrom(msg.sender, address(this), _amount)==true, "cannot transfer TBs to the exchange");
-        TokenBalance["TB"] += _amount;
-        //tranfer _amount of TAs to msg.sender (the buyer)
-        require(proxyTA.transfer(msg.sender, _amount)==true, "cannot transfer TAs to the buyer");
-        TokenBalance["TA"] -= _amount;
-        
-        return true;
-    }
 
-    //swaps B tokens for A 1:1
-    function buyB(uint _amount) public returns(bool){
-        //transfer _amount of TAs from msg.sender to TokenExchange
-        //allowance is checked inside transferFrom
-        require(proxyTA.transferFrom(msg.sender, address(this), _amount)==true, "cannot transfer TAs to the exchange");
-        TokenBalance["TA"] += _amount;
-        //tranfer _amount of TBs to msg.sender (the buyer)
-        require(proxyTB.transfer(msg.sender, _amount)==true, "cannot transfer TBs to the buyer");
-        TokenBalance["TB"] -= _amount;
-        
-        return true;
+    function buyToken(address _TA, address _TB, uint _amount) public returns(uint){
+        //check which token is wanted  
+        bool _tokenA = _TA == address(proxyTA); //buy TA for TB
+        bool _tokenB = _TB == address(proxyTB); //buy TB for TA
+        uint _tokens_transfered = 0;
+        if (_tokenA) { //if TA is wanted
+            //transfer _amount of TBs from msg.sender to TokenExchange
+            proxyTB.safeTransferFrom(msg.sender, address(this), _amount);
+            TokenBalance["TB"] += _amount;
+            //transfer _amount of TAs to msg.sender (the buyer)
+            require(proxyTA.transfer(msg.sender, _amount), "cannot transfer TAs to the buyer");
+            TokenBalance["TA"] -= _amount;
+            _tokens_transfered = _amount;
+        }
+        if (_tokenB) { //if TB is wanted
+            //transfer _amount of TAs from msg.sender to TokenExchange
+            proxyTA.safeTransferFrom(msg.sender, address(this), _amount);
+            TokenBalance["TA"] += _amount;
+            //transfer _amount of TBs to msg.sender (the buyer)
+            require(proxyTA.transfer(msg.sender, _amount), "cannot transfer TBs to the buyer");
+            TokenBalance["TB"] -= _amount;
+            _tokens_transfered = _amount;
+        }      
+        return _tokens_transfered; 
     }    
-
-    function showBalance(string memory TokenCode) public view returns(uint){
-
-    }
 
     
 }
